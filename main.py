828b52fe4c62a5e82d39cec7102e7666a33d70f8
Hunter Schneider <schnehunter@gmail.com>
2026-01-28 13:51:27 -0600

    commit

--- main.py
+++ main.py
@@ -11,9 +11,16 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def home(request: Request):
     regen = random.randint(1, 1000000000)
-    return templates.TemplateResponse("index.html", {"request": request, "name": "World", "color": "orange", "regen": regen, "first": first})
+    return templates.TemplateResponse("index.html", {"request": request, "name": "Welcome", "color": "green", "regen": regen, "first": first})
+@app.get("/gallery")
+def home(request: Request):
+    return templates.TemplateResponse("gallery.html", {"request": request, "color": "green",})
+@app.get("/about")
+def home(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request, "color": "green",})
 @app.get("/contact")
 def home(request: Request):
-    return templates.TemplateResponse("contact.html", {"request": request, "email": "snider@snider.com"})
+    return templates.TemplateResponse("contact.html", {"request": request, "email": "snider@snider.com", "color": "green",})
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
